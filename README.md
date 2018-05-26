55d490c21609e68233cbcb99d6cbb7106b875ba0
YellPika <yellpika@gmail.com>
2018-05-25 20:02:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# latex-input
+
+This extension allows you to enter Unicode symbols using LaTeX-like commands. Available commands are added to the list of auto-completions when you type `\`.
---
+++ src/extension.ts
@@ -0,0 +1,91 @@
+'use strict';
+
+import { readFileSync } from 'fs';
+import {
+  ExtensionContext,
+  commands,
+  languages,
+  workspace,
+  Range,
+  CompletionItemProvider,
+  CompletionItem,
+  CompletionItemKind
+} from 'vscode';
+
+export function activate(context: ExtensionContext) {
+  type Mappings = { [key: string]: string };
+
+  let mappings: Mappings;
+
+  function reloadMappings() {
+    function load(obj: string | Mappings | (string | Mappings)[]): Mappings {
+      if (typeof obj === "string") {
+        let filename = context.asAbsolutePath(obj);
+        return JSON.parse(readFileSync(filename).toString());
+      } else if (obj instanceof Array) {
+        let result: Mappings = {};
+        obj.forEach(value => {
+          result = { ...result, ...load(value) };
+        });
+        return result;
+      } else {
+        return obj;
+      }
+    }
+
+    let config = workspace.getConfiguration('latex-input');
+    mappings = load(config.mappings);
+  }
+
+  let completionProvider: CompletionItemProvider = {
+    async provideCompletionItems(document, position, token, context) {
+      let config = workspace.getConfiguration('latex-input');
+      if (config.triggers.indexOf(context.triggerCharacter) === -1) {
+        return [];
+      }
+
+      let range = new Range(position.translate(0, -1), position);
+      let completions: Array<CompletionItem> = [];
+      for (const from in mappings) {
+        let to: string = mappings[from];
+        let item = new CompletionItem('\\' + from);
+        item.detail = to;
+        item.insertText = to;
+        item.kind = CompletionItemKind.Text;
+        item.range = range;
+        completions.push(item);
+      }
+
+      return completions;
+    }
+  };
+
+  function registerCompletionProvider() {
+    let config = workspace.getConfiguration('latex-input');
+    return languages.registerCompletionItemProvider(
+      config.selector,
+      completionProvider,
+      config.triggers);
+  }
+
+  reloadMappings();
+
+  let registration = registerCompletionProvider();
+
+  context.subscriptions.push(
+    workspace.onDidChangeConfiguration((e) => {
+      if (e.affectsConfiguration("latex-input.mappings")) {
+        reloadMappings();
+      }
+
+      if (e.affectsConfiguration("latex-input.selector")) {
+        registration.dispose();
+        registration = registerCompletionProvider();
+      }
+    }));
+
+  context.subscriptions.push(
+    commands.registerCommand(
+      'latex-input.reload-mappings',
+      reloadMappings));
+}
