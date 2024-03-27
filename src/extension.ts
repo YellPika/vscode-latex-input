2024d486db33b3b144f4a9b947d5736d89068b32
YellPika <yellpika@gmail.com>
2024-03-27 12:37:42 -0400

    feat!: extension can be enabled/disabled on a per-language basis mappings are loaded on demand

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,6 @@
 import { readFileSync } from 'fs';
 import {
   ExtensionContext,
-  commands,
   languages,
   workspace,
   Range,
@@ -13,48 +12,37 @@ import {
 } from 'vscode';
 
 export function activate(context: ExtensionContext) {
-  type Mappings = { [key: string]: string };
-
-  let mappings: Mappings;
-
-  function reloadMappings() {
-    function load(obj: string | Mappings | (string | Mappings)[]): Mappings {
-      if (typeof obj === "string") {
-        let filename = context.asAbsolutePath(obj);
-        return JSON.parse(readFileSync(filename).toString());
-      } else if (obj instanceof Array) {
-        let result: Mappings = {};
-        obj.forEach(value => {
-          result = { ...result, ...load(value) };
-        });
-        return result;
-      } else {
-        return obj;
-      }
-    }
-
-    let config = workspace.getConfiguration('latex-input');
-    mappings = load(config.mappings);
-  }
-
   let completionProvider: CompletionItemProvider = {
-    async provideCompletionItems(document, position, token, context) {
-      let config = workspace.getConfiguration('latex-input');
-      if (config.triggers.indexOf(context.triggerCharacter) === -1) {
+    async provideCompletionItems(document, position, token, {triggerCharacter}) {
+      let config = workspace.getConfiguration('latex-input', document);
+      if (!config.enabled || config.triggers.indexOf(triggerCharacter) === -1) {
         return [];
       }
 
       let range = new Range(position.translate(0, -1), position);
       let completions: Array<CompletionItem> = [];
-      for (const from in mappings) {
-        let to: string = mappings[from];
-        let item = new CompletionItem(context.triggerCharacter + from);
-        item.detail = to;
-        item.kind = CompletionItemKind.Text;
-        item.insertText = to;
-        item.range = range;
-        completions.push(item);
+
+      type Mappings = { [key: string]: string };
+      function load(obj: string | Mappings | (string | Mappings)[]) {
+        if (typeof obj === "string") {
+          let filename = context.asAbsolutePath(obj);
+          load(JSON.parse(readFileSync(filename).toString()));
+        } else if (obj instanceof Array) {
+          obj.forEach(load);
+        } else {
+          for (const from in obj) {
+            let to = obj[from];
+            let item = new CompletionItem(triggerCharacter + from);
+            item.detail = to;
+            item.kind = CompletionItemKind.Text;
+            item.insertText = to;
+            item.range = range;
+            completions.push(item);
+          }
+        }
       }
+
+      load(config.mappings);
 
       return completions;
     }
@@ -63,30 +51,19 @@ export function activate(context: ExtensionContext) {
   function registerCompletionProvider() {
     let config = workspace.getConfiguration('latex-input');
     return languages.registerCompletionItemProvider(
-      config.selector,
+      [{ "scheme": "file" }, { "scheme": "untitled" }],
       completionProvider,
       ...config.triggers);
   }
-
-  reloadMappings();
 
   let registration = registerCompletionProvider();
 
   context.subscriptions.push(
     workspace.onDidChangeConfiguration((e) => {
-      if (e.affectsConfiguration("latex-input.mappings")) {
-        reloadMappings();
-      }
-
       if (e.affectsConfiguration("latex-input.selector") ||
-          e.affectsConfiguration("latex-input.triggers")) {
+        e.affectsConfiguration("latex-input.triggers")) {
         registration.dispose();
         registration = registerCompletionProvider();
       }
     }));
-
-  context.subscriptions.push(
-    commands.registerCommand(
-      'latex-input.reload-mappings',
-      reloadMappings));
 }
